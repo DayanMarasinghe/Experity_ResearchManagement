11364d149c394123027a9761ffabd49abdc15a59
Ishini Kiridena <ishini.kiridenaedu@gmail.com>
2022-05-26 07:27:45 +0530

    Initial UI changes added

---
+++ frontend/components/shared/header.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Collapse, Nav, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from "reactstrap";
+
+function Header(){
+    return(
+        <div>
+            <Navbar color="light"
+                expand="md">
+                <NavbarBrand>
+                    <h2><b>Experity</b></h2>
+                </NavbarBrand>
+                <NavbarToggler onClick={function noRefCheck() { }}></NavbarToggler>
+                <Collapse navbar>
+                    <Nav className="me-auto"
+                        navbar>
+                        <NavItem>
+                            <NavLink href="/supervisordashboard">
+                                Supervisor Dashboard
+                            </NavLink>
+                        </NavItem>
+                    </Nav>
+                </Collapse>
+            </Navbar>
+        </div>
+        
+    );
+}
+
+export default Header;
---
+++ frontend/components/supervisorDashboard.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class SupervisorDashboard extends Component{
+    render(){
+        return(
+            <div>Hello Supervisor</div>
+        );
+    }
+}
+
+export default SupervisorDashboard;
