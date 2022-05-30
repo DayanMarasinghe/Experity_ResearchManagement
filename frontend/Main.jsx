ed88cea9a27a8f1bddf610a0f44e66b7cd7bdf50
Ishini Kiridena <ishini.kiridenaedu@gmail.com>
2022-05-30 23:34:51 +0530

    Dashboard UI completed

--- frontend/Main.jsx
+++ frontend/Main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, BrowserRouter as Router, Switch, Routes } from "react-router-dom";
-import SupervisorDashboard from './components/supervisorDashboard';
+import SupervisorDashboard from './components/supervisor-view/supervisorDashboard'
 import Header from './components/shared/header';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
