40b9fcfae7dc88a0dcc2ce9d15fb821d328966d3
Ishini Kiridena <ishini.kiridenaedu@gmail.com>
2022-06-01 21:00:19 +0530

    Group view UI completed

--- frontend/Main.jsx
+++ frontend/Main.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import { Route, BrowserRouter as Router, Switch, Routes } from "react-router-dom";
-
-
 import UserLogin from './components/userLogin';
-
 import SupervisorDashboard from './components/supervisor-view/supervisorDashboard'
-
 import Header from './components/shared/header';
 import StudentReg from './components/studentRegister';
 import StaffReg from './components/staffRegister';
 import UserReg from './components/userRegister';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import GroupView from './components/supervisor-view/groupview';
 
 export default class App extends React.Component {
     constructor(props) {
@@ -27,6 +24,7 @@ export default class App extends React.Component {
                         <Route path='/studentreg' element={<StudentReg/>}/>
                         <Route path='/userreg' element={<UserReg/>}/>
                         <Route path='/staffreg' element={<StaffReg/>}/>
+                        <Route path='/group' element={<GroupView />} />
                     </Routes>
                 </Router>
             </div>
